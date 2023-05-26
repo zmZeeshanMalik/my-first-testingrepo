0bb8529e8d1773a974536dceb777732856cc193e
devzeemalik <devzeeshanmalik@gmail.com>
2023-05-26 17:32:18 +0500

    'first-commit'

---
+++ arjun-store-description.php
@@ -0,0 +1,59 @@
+1 : App introduction.
+        Create, schedule, and have awesome blog posts and google web stories on your store.
+2 : App details
+        Get website visitors from Google! Improve your SEO by using Google Web Stories. Schedule stories and blog posts with beautiful imagery, text and call to action buttons directly into search results!
+Small businesses, communicate across various mediums in one single post with AI-powered wording and emoji suggestions.   
+
+3 : Feature 1
+    Create and schedule stories on your store with imagery, text and call to action.
+
+4 : Feature 2
+    Create and schedule blog posts on your store
+
+5 : Feature 3
+    Get website visitors from Google! Improve your SEO by using Google Web Stories.
+
+6 : demo store url
+        https://vimeo.com/718783442
+
+7 : Feature image alt text.
+        HelloWoofy logo and design.
+
+
+8 : sereenshot 1:
+        On the main page of the journal, you can view your blogs.
+9 : screenshot 2:
+        When writing a blog, you can add emojis, quotes, and images.
+10 : screenshot 3:
+        Schedule your blog out to Shopify and other blog platforms.
+11 : screenshot 4:
+        Add HelloWoofy to your Shopify website and blog.
+
+12 : App card subtitle:
+        Smart Marketing for Underdogs. Create. Schedule. Post. 
+
+
+13 : Testing instructions:
+
+HelloWoofy test login:
+Website: https://woofy-demo.fluvius.co/
+Username: atraila762+6040@gmail.com
+Password: test
+
+Video: https://vimeo.com/718783442
+Steps:
+1. Install the HelloWoofy.com app to your Shopify store.
+2. Go to settings and copy the API Key.
+3. Go to HelloWoofy.com and sign in.
+4. Navigate to "Settings" towards the top right.
+5. Click "Add account to this Calendar".
+6. Click "Add Shopify Account".
+7. Click "Add Blog Account"
+8. Insert Store Name.
+9. Paste the API Key.
+10. Change any additional settings.
+11. Click "Add"
+12. Go to the Journal tab.
+13. Create a blog post.
+14. Click "Upload" and select your Shopify store.
+15. Check that it is posted to Shopify.                                                                
